ba9b2975e989f3a4bfe88b2b245f5bbdca8447e1
ToCodeABluejay <73219116+ToCodeABluejay@users.noreply.github.com>
2022-12-30 23:12:41 -0500

    Update Harmony.java

--- planning/Harmony.java
+++ planning/Harmony.java
@@ -2,38 +2,36 @@ package planning;
 import java.io.*;
 import java.util.*;
 /** A class to compute the harmony function described in Towards a New Socialism
-  <p>
-       Copyright (C) 2018 William Paul Cockshott
-
-       This program is free software: you can redistribute it and/or modify
-       it under the terms of the GNU General Public License as published by
-       the Free Software Foundation, either version 3 of the License, or
-       (at your option) any later version.
-
-       This program is distributed in the hope that it will be useful,
-       but WITHOUT ANY WARRANTY; without even the implied warranty of
-       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-       GNU General Public License for more details.
-
-       You should have received a copy of the GNU General Public License
-       along with this program.  If not, see https://www.gnu.org/licenses/.
-    * */
+ *
+ *   Copyright (C) 2018 William Paul Cockshott
+ *   Copyright (C) 2022 Gabriel James Bauer
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
 public class Harmony {
-
+    public static double H(double target, double netoutput)
     /** the harmony function itself */
-    public static double H(double target, double netoutput) {
-
+    {
         double scale =  (netoutput-target  )/target;
-        //    System.out.println("H("+target+","+netoutput+") scale "+scale);
         if (scale<0) return scale - (scale*scale)*0.5;
         return Math.log(scale+1);
-
-
     }
+    
+    public static double dH(double target, double netoutput)
     /** the derivative of the harmony function
      * evaluated numerically so as to be independent of the H function */
-    public static double dH(double target, double netoutput) {
-
+    {
         double epsilon = 0.000001;
         double base = H(target,netoutput);
         double basePlusEpsilon = H(target, epsilon+netoutput);
